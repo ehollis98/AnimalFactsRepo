6597163e2bd42fb5797939905a223329060062a2
Charles Clark <cclark5@madisoncollege.edu>
2018-11-12 15:21:53 -0600

    Added DAO and sessionFactory.

--- src/main/java/persistence/FactDAO.java
+++ src/main/java/persistence/FactDAO.java
@@ -1,5 +1,28 @@
 package persistence;
 
+import entity.Fact;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+
+
 public class FactDAO {
     
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    public List<Fact> getAllFacts() {
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Fact> query = builder.createQuery(Fact.class);
+        Root<Fact> root = query.from(Fact.class);
+        List<Fact> facts = session.createQuery(query).getResultList();
+        session.close();
+        return facts;
+    }
+
 }
